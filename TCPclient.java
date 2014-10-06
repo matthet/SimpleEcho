75b0dcc0718fc987cfcc1693fa2f271bceefabc0
Tara <matthet@tcd.ie>
2014-10-06 23:27:14 +0100

    TCPclient Code (Tidied)

--- TCPclient.java
+++ TCPclient.java
@@ -6,20 +6,17 @@ class TCPClient{
    public static void main(String argv[]) throws Exception {
 
       System.out.println('\n' + "Type a Word: ");
-      BufferedReader inFromUser = new BufferedReader(new 
-InputStreamReader(System.in));
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
       String word = inFromUser.readLine();
       System.out.println();
 
       String path = "GET /echo.php?message=" + word + " HTTP/1.0\r\n";
 
       Socket clientSocket = new Socket("localhost",8888);
-      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-true);
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
       out.println(path);
 
-      BufferedReader in = new BufferedReader( new 
-InputStreamReader(clientSocket.getInputStream()));
+      BufferedReader in = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
 
       String fromServer;
       while((fromServer = in.readLine()) != null){
